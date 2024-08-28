4a17204b085a5cd1bcb61daa484565db7fd6a75e
bluemoon983 <msdrobot@naver.com>
2024-08-28 21:14:28 +0900

    writing-page

--- lib/ui/writing_ui.dart
+++ lib/ui/writing_ui.dart
@@ -7,7 +7,29 @@ class WritingWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.white,
-      body: Text("writing"),
+      body: Padding(
+        padding: EdgeInsets.only(left: 20.0, top: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "내 물건 팔기",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

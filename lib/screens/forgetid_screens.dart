7ebc7126b6dcf715b4d1c8dedd1695e398ce35ef
bluemoon983 <msdrobot@naver.com>
2024-09-10 23:38:54 +0900

    새 스크린

---
+++ lib/screens/forgetid_screens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForgetidScreens extends StatelessWidget {
+  const ForgetidScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/signup_screens.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignupScreens extends StatelessWidget {
+  const SignupScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

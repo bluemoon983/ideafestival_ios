49f3dd96e55bfa7e5aac0f76b5ac2e4b55baa9da
bluemoon983 <msdrobot@naver.com>
2024-09-10 23:21:46 +0900

    login

--- lib/screens/login_screens.dart
+++ lib/screens/login_screens.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 class LoginScreens extends StatelessWidget {
@@ -11,155 +9,157 @@ class LoginScreens extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
+          padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
                 "Login",
                 style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w800,
                 ),
               ),
               const SizedBox(height: 20),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Email address",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
               const TextField(
                 decoration: InputDecoration(
                   contentPadding: EdgeInsets.symmetric(
-                    vertical: 10.0, // Adjusts the height of the search bar
+                    vertical: 10.0,
                     horizontal: 10.0,
                   ),
                   hintText: "name@example.com",
                   hintStyle: TextStyle(color: Colors.black54, fontSize: 16.0),
                   filled: true,
-                  fillColor: Colors.white, // Background color
+                  fillColor: Colors.white,
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color
-                      width: 2.0,
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
                   ),
                   enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color when enabled
-                      width: 2.0,
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color when focused
-                      width: 2.0,
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 20,
+              const SizedBox(height: 20),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Username/ID",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
               ),
               const TextField(
                 decoration: InputDecoration(
                   contentPadding: EdgeInsets.symmetric(
-                    vertical: 10.0, // Adjusts the height of the search bar
+                    vertical: 10.0,
                     horizontal: 10.0,
                   ),
                   hintText: "Enter your username or ID",
                   hintStyle: TextStyle(color: Colors.black54, fontSize: 16.0),
                   filled: true,
-                  fillColor: Colors.white, // Background color
+                  fillColor: Colors.white,
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color
-                      width: 2.0,
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
                   ),
                   enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color when enabled
-                      width: 2.0,
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color when focused
-                      width: 2.0,
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
                   ),
                 ),
               ),
-              const SizedBox(
-                height: 20,
+              const SizedBox(height: 20),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Password",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
               ),
               const TextField(
+                obscureText: true, // Hide the text for password input
                 decoration: InputDecoration(
                   contentPadding: EdgeInsets.symmetric(
-                    vertical: 10.0, // Adjusts the height of the search bar
+                    vertical: 10.0,
                     horizontal: 10.0,
                   ),
                   hintText: "Enter your password",
                   hintStyle: TextStyle(color: Colors.black54, fontSize: 16.0),
                   filled: true,
-                  fillColor: Colors.white, // Background color
+                  fillColor: Colors.white,
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color
-                      width: 2.0,
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
                   ),
                   enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
-                    ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color when enabled
-                      width: 2.0,
-                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: 500,
+                height: 50,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    side: const BorderSide(color: Colors.black),
+                    backgroundColor: Colors.black,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
                     ),
-                    borderSide: BorderSide(
-                      color: Colors.black, // Border color when focused
-                      width: 2.0,
+                  ),
+                  onPressed: () => _navigateToMain(context),
+                  child: const Text(
+                    "Sign in",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.black, // Text color
-                  side: const BorderSide(color: Colors.black), // Border color
-                ),
-                onPressed: () => _navigateToMain(context),
-                child: const Text(
-                  "엄 준 식",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
+              const SizedBox(height: 20),
+              const Row(
+                children: [
+                  Text("UmJunSick"),
+                  SizedBox(
+                    width: 20,
                   ),
-                ),
-              ),
+                  Text("Ralo"),
+                ],
+              )
             ],
           ),
         ),
@@ -169,6 +169,6 @@ class LoginScreens extends StatelessWidget {
 
   void _navigateToMain(BuildContext context) {
     Navigator.popUntil(
-        context, (route) => route.isFirst); // Navigates to the first route
+        context, (route) => route.isFirst); // Navigate to the first route
   }
 }

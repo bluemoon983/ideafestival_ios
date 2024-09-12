64d687f9042bcf73048c63445c5c99531899adb2
bluemoon983 <msdrobot@naver.com>
2024-09-12 22:56:20 +0900

    채팅 정상화

--- lib/screens/chat_screens.dart
+++ lib/screens/chat_screens.dart
@@ -13,28 +13,33 @@ class _ChatScreensState extends State<ChatScreens> {
   final List<Map<String, String>> _messages = [];
   final TextEditingController _controller = TextEditingController();
   final ScrollController _scrollController = ScrollController();
+  final FocusNode _focusNode = FocusNode();
 
   void _sendMessage() {
-    if (_controller.text.isEmpty || _controller.text.trim().length == 0) return;
+    if (_controller.text.trim().isEmpty) return;
 
     final now = DateTime.now();
     final formattedTime = DateFormat('HH:mm').format(now);
 
-    setState(
-      () {
-        _messages.add({
-          'text': _controller.text,
-          'time': formattedTime,
-        });
-        _controller.clear();
-      },
-    );
+    setState(() {
+      _messages.add({
+        'text': _controller.text,
+        'time': formattedTime,
+      });
+      _controller.clear();
+    });
 
-    _scrollController.animateTo(
-      _scrollController.position.maxScrollExtent + 50,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeOut,
-    );
+    _scrollToBottom();
+  }
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
   }
 
   @override
@@ -52,38 +57,16 @@ class _ChatScreensState extends State<ChatScreens> {
               itemCount: _messages.length,
               itemBuilder: (context, index) {
                 final message = _messages[index];
-                return Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          message['text']!,
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          message['time']!,
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 10),
-                        ),
-                      ],
-                    ),
-                  ),
+                return _MessageWidget(
+                  text: message['text']!,
+                  time: message['time']!,
                 );
               },
             ),
           ),
           _BottomInputField(
             controller: _controller,
+            focusNode: _focusNode,
             onSend: _sendMessage,
           ),
         ],
@@ -92,25 +75,68 @@ class _ChatScreensState extends State<ChatScreens> {
   }
 }
 
+class _MessageWidget extends StatelessWidget {
+  final String text;
+  final String time;
+
+  const _MessageWidget({
+    required this.text,
+    required this.time,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerRight, // Align messages to the right
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        decoration: BoxDecoration(
+          color: Colors.black, // Changed to blue for better visibility
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment:
+              CrossAxisAlignment.end, // Align text and time to the end
+          children: [
+            Text(
+              text,
+              style: const TextStyle(color: Colors.white),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              time,
+              style: const TextStyle(color: Colors.white, fontSize: 10),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class _BottomInputField extends StatelessWidget {
   final TextEditingController controller;
+  final FocusNode focusNode;
   final VoidCallback onSend;
 
   const _BottomInputField({
     required this.controller,
+    required this.focusNode,
     required this.onSend,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 40.0),
+      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 16.0),
       color: Colors.white,
       child: Row(
         children: <Widget>[
           Expanded(
             child: TextField(
               controller: controller,
+              focusNode: focusNode,
               decoration: const InputDecoration.collapsed(
                 hintText: '메시지 작성....',
               ),

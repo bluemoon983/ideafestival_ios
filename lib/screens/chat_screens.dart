aa3cc1384fe18557023ab26b994835fa14bcdcd1
bluemoon983 <msdrobot@naver.com>
2024-10-15 15:51:50 +0900

    오류의 정상화

--- lib/screens/chat_screens.dart
+++ lib/screens/chat_screens.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:intl/intl.dart';
-import 'dart:math';
 
 class ChatScreens extends StatefulWidget {
   final String name;
@@ -21,46 +19,34 @@ class _ChatScreensState extends State<ChatScreens> {
     if (text.trim().isEmpty) return;
 
     final now = DateTime.now();
-    final formattedTime = DateFormat('HH:mm').format(now);
-
     final message = types.TextMessage(
       author: _user,
       createdAt: now.millisecondsSinceEpoch,
-      id: Random().nextInt(1000).toString(), // unique ID for each message
+      id: DateTime.now().millisecondsSinceEpoch.toString(), // 고유 ID 생성
       text: text,
     );
 
     setState(() {
       _messages.insert(0, message);
     });
+
+    _controller.clear(); // 메시지 전송 후 입력 필드 비우기
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.name),
+        title: Text(widget.name,
+            style: const TextStyle(color: Colors.black)), // 타이틀 색상 설정
         backgroundColor: Colors.white,
       ),
-      body: SafeArea(
-        // SafeArea 추가
-        child: Column(
-          children: [
-            Expanded(
-              // Expanded로 감싸기
-              child: _messages.isEmpty
-                  ? const Center(
-                      child: Text('No messages yet!')) // 메시지가 없을 때 기본 텍스트 표시
-                  : Chat(
-                      messages: _messages,
-                      onSendPressed: (partialText) {
-                        _sendMessage(partialText.text);
-                      },
-                      user: _user,
-                    ),
-            ),
-          ],
-        ),
+      body: Chat(
+        messages: _messages,
+        onSendPressed: (partialText) {
+          _sendMessage(partialText.text);
+        },
+        user: _user,
       ),
     );
   }

720ccbd3faa31a18398c1d2b48a2ed9a037ade25
bluemoon983 <msdrobot@naver.com>
2024-10-21 13:45:37 +0900

    사용자 대화 저장

--- lib/screens/chat_screens.dart
+++ lib/screens/chat_screens.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:iosgsmarket/src/repo/chat_database.dart';
 
 class ChatScreens extends StatefulWidget {
   final String name;
@@ -14,15 +15,29 @@ class _ChatScreensState extends State<ChatScreens> {
   final List<types.Message> _messages = [];
   final types.User _user = const types.User(id: 'user');
   final TextEditingController _controller = TextEditingController();
+  final ChatDatabase _chatDatabase = ChatDatabase(); // 데이터베이스 인스턴스
 
-  void _sendMessage(String text) {
+  @override
+  void initState() {
+    super.initState();
+    _loadMessages(); // 기존 메시지 로드
+  }
+
+  Future<void> _loadMessages() async {
+    final messages = await _chatDatabase.getMessages();
+    setState(() {
+      _messages.addAll(messages.reversed); // 최신 메시지부터 보여주기 위해 역순으로 추가
+    });
+  }
+
+  void _sendMessage(String text) async {
     if (text.trim().isEmpty) return;
 
     final now = DateTime.now();
     final message = types.TextMessage(
       author: _user,
       createdAt: now.millisecondsSinceEpoch,
-      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      id: now.millisecondsSinceEpoch.toString(),
       text: text,
     );
 
@@ -31,6 +46,9 @@ class _ChatScreensState extends State<ChatScreens> {
     });
 
     _controller.clear();
+
+    // 메시지를 데이터베이스에 저장
+    await _chatDatabase.insertMessage(message);
   }
 
   @override

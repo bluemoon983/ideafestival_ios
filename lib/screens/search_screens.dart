a44e84e285b5f93658d62935c59800cd3a4c2f69
bluemoon983 <msdrobot@naver.com>
2024-10-31 16:43:38 +0900

    검색

--- lib/screens/search_screens.dart
+++ lib/screens/search_screens.dart
@@ -12,31 +12,16 @@ class SearchScreens extends StatefulWidget {
 
 class _SearchScreensState extends State<SearchScreens> {
   final _searchController = TextEditingController();
-  Product? _searchResult;
+  List<Product> _searchResults = [];
 
   Future<void> _searchProduct(String query) async {
     // 검색어로 DB 조회
     List<Product> products = await SqlProductCrud.searchProducts(query);
 
-    // 검색 결과가 있을 경우
-    if (products.isNotEmpty) {
-      setState(() {
-        _searchResult = products.first; // 가장 유사한 상품을 결과로 설정
-      });
-
-      // 상세 페이지로 이동
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => DetailView(product: _searchResult!),
-        ),
-      );
-    } else {
-      // 검색 결과가 없을 경우
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('검색 결과가 없습니다.')),
-      );
-    }
+    // 검색 결과가 업데이트될 때 setState 호출
+    setState(() {
+      _searchResults = products;
+    });
   }
 
   @override
@@ -45,23 +30,10 @@ class _SearchScreensState extends State<SearchScreens> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Hero(
-          tag: "titlename",
-          flightShuttleBuilder: (flightContext, animation, flightDirection,
-              fromHeroContext, toHeroContext) {
-            return ScaleTransition(
-              scale: animation.drive(
-                Tween(begin: 0.9, end: 1.0)
-                    .chain(CurveTween(curve: Curves.easeInOut)),
-              ),
-              child: Material(
-                color: Colors.transparent,
-                child: toHeroContext.widget,
-              ),
-            );
-          },
-          child: const Material(
-            color: Colors.transparent,
+        title: const Padding(
+          padding: EdgeInsets.only(top: 30.0, bottom: 20),
+          child: Align(
+            alignment: Alignment.centerLeft,
             child: Text(
               "GSMarket",
               style: TextStyle(
@@ -75,16 +47,11 @@ class _SearchScreensState extends State<SearchScreens> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(
-                height: 20,
-              ),
-              // Hero with Custom Animation
+              const SizedBox(height: 20),
               Hero(
                 tag: "searchBarHero",
                 flightShuttleBuilder: (flightContext, animation,
@@ -104,14 +71,12 @@ class _SearchScreensState extends State<SearchScreens> {
                   color: Colors.transparent,
                   child: TextField(
                     controller: _searchController, // 검색어 입력 필드
-                    onSubmitted: (query) {
-                      _searchProduct(query); // 검색어 제출 시 검색 수행
+                    onChanged: (query) {
+                      _searchProduct(query); // 입력이 변경될 때마다 검색 수행
                     },
                     decoration: const InputDecoration(
                       contentPadding: EdgeInsets.symmetric(
-                        vertical: 10.0,
-                        horizontal: 10.0,
-                      ),
+                          vertical: 10.0, horizontal: 10.0),
                       prefixIcon: Icon(Icons.search, color: Colors.black),
                       hintText: "검색.... ",
                       hintStyle:
@@ -119,36 +84,47 @@ class _SearchScreensState extends State<SearchScreens> {
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10.0),
-                        ),
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                          width: 2.0,
-                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(color: Colors.black, width: 2.0),
                       ),
                       enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10.0),
-                        ),
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                          width: 2.0,
-                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(color: Colors.black, width: 2.0),
                       ),
                       focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10.0),
-                        ),
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                          width: 2.0,
-                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(color: Colors.black, width: 2.0),
                       ),
                     ),
                   ),
                 ),
               ),
+              const SizedBox(height: 20),
+              // 검색 결과 리스트
+              _searchResults.isNotEmpty
+                  ? ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: _searchResults.length,
+                      itemBuilder: (context, index) {
+                        final product = _searchResults[index];
+                        return ListTile(
+                          title: Text(product.name),
+                          subtitle: Text('${product.price.toInt()}원'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    DetailView(product: product),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    )
+                  : const Center(
+                      child: Text('검색 결과가 없습니다.')), // 검색 결과가 없을 경우 메시지
             ],
           ),
         ),

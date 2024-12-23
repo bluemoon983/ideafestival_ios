391dc0b9dc9f091c29c3c5d1b9309f6a675b6176
bluemoon983 <msdrobot@naver.com>
2024-12-23 19:40:55 +0900

    스플래시

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,39 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MainScreens(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToMain();
+  }
+
+  Future<void> _navigateToMain() async {
+    await Future.delayed(const Duration(seconds: 3)); // 3초 대기
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const MainScreens()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('lib/assets/Ralo.png'), // 스플래시 로고
+      ),
     );
   }
 }

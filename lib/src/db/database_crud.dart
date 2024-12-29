dc76b9ade39f0e4cf8bcd3e3def71c6da91f8356
bluemoon983 <msdrobot@naver.com>
2024-12-30 08:38:23 +0900

    crud

---
+++ lib/src/db/database_crud.dart
@@ -0,0 +1,77 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class ProductDatabase {
+  static final ProductDatabase instance = ProductDatabase._init();
+  static Database? _database;
+
+  ProductDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('products.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textType = 'TEXT NOT NULL';
+    const boolType = 'BOOLEAN NOT NULL';
+    const intType = 'INTEGER NOT NULL';
+
+    await db.execute('''
+    CREATE TABLE products (
+      id $idType,
+      name $textType,
+      description $textType,
+      price $intType,
+      isWished $boolType
+    )
+    ''');
+  }
+
+  Future<int> createProduct(Map<String, dynamic> product) async {
+    final db = await instance.database;
+    return await db.insert('products', product);
+  }
+
+  Future<List<Map<String, dynamic>>> readAllProducts() async {
+    final db = await instance.database;
+    return await db.query('products');
+  }
+
+  Future<int> updateProduct(Map<String, dynamic> product) async {
+    final db = await instance.database;
+    return await db.update(
+      'products',
+      product,
+      where: 'id = ?',
+      whereArgs: [product['id']],
+    );
+  }
+
+  Future<int> deleteProduct(int id) async {
+    final db = await instance.database;
+    return await db.delete(
+      'products',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
